1f04a1fe0d5db1c03df7702060692a51da5e75dc
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-09-17 16:03:44 -0500

    Added Customer entity and CustomerMapper using MapStruct

--- target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-15T14:34:05-0500",
+    date = "2020-09-17T16:01:14-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 12.0.1 (Oracle Corporation)"
 )
 @Component
